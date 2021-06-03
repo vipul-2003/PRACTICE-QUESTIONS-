64bc338b3c1f8c1885549ed52337b15b3430455c
vipul-2003 <72446442+vipul-2003@users.noreply.github.com>
2021-06-03 13:22:42 +0530

    Update kth_element_in_sorted_matrix.cpp

--- kth_element_in_sorted_matrix.cpp
+++ kth_element_in_sorted_matrix.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main ()
 {
-    int n ;
+    int n ; // To get the order for a matrix 
     cin>>n;
 
     int matrix[n][n];
@@ -15,12 +15,12 @@ int main ()
     {
         for (int j = 0 ; j< n ; j++)
         {
-            cin>>matrix[i][j];
+            cin>>matrix[i][j];  // To get the elements of matrix from the user 
         }
     }
 
     int k ;
-    cin >>k;
+    cin >>k; // To get the number (integer type ) to find the element at that place 
 
     int row = k / n ;
     int column = (k % n) - 1 ;
